e20766dc546f32bca7e1c28744768d0e44667128
김태영 <qwez0498@naver.com>
2023-04-18 15:02:21 +0900

    First Commit

---
+++ Spring_MVC_Board_kty/src/main/java/com/itwillbs/spring_mvc_board_kty/mapper/BoardMapper.java
@@ -0,0 +1,10 @@
+package com.itwillbs.spring_mvc_board_kty.mapper;
+
+import com.itwillbs.spring_mvc_board_kty.vo.*;
+
+public interface BoardMapper {
+
+	// 글 쓰기
+	int insertBoard(BoardVO board);
+
+}
